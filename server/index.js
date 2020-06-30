949314b06aeb72fb8e725c99a878cc39a6ca8541
Adrian <vent.adrian@gmail.com>
2020-06-30 16:26:46 -0700

    proxy complete

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const path = require('path');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
-app.use('/', express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../client')));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.all('/calendar', createProxyMiddleware({ target: 'localhost:3001' }));
 app.all('/reviews', createProxyMiddleware({ target: 'localhost:3002' }));
