14e29c924f8144e9c4b74afe388fca0edb3bdcef
Adrian <vent.adrian@gmail.com>
2020-06-30 14:21:08 -0700

    added all files

--- server/index.js
+++ server/index.js
@@ -1,22 +1,17 @@
 const express = require('express');
 const path = require('path');
-const proxy = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
-const calendarProxy = proxy('/calendar', {target: 'localhost:3001'});
-const reviewsProxy = proxy('/reviews', {target: 'localhost:3002'});
-const carouselProxy = proxy('/carousel', {target: 'localhost:3003'});
-const photosProxy = proxy('/photoGallery', {target: 'localhost:3004'});
-
-app.use('/:roomId', express.static(path.join(__dirname, '../client/dist')));
+app.use('/', express.static(path.join(__dirname, '../client/dist')));
 app.use(bodyParser.json());
 
-app.use(calendarProxy)
-app.use(reviewsProxy);
-app.use(carouselProxy);
-app.use(photosProxy);
+app.all('/calendar', createProxyMiddleware({ target: 'localhost:3001' }));
+app.all('/reviews', createProxyMiddleware({ target: 'localhost:3002' }));
+app.all('/carousel', createProxyMiddleware({ target: 'localhost:3003' }));
+app.all('/photogallery', createProxyMiddleware({ target: 'localhost:3004' }));
 
-app.listen(port);
+app.listen(port, () => console.log(`Listening at http://localhost:${port}/`));
