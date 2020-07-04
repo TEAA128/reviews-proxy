5da35a3a4d540e1a693a3a55c55ea9c3922c0be8
Adrian <vent.adrian@gmail.com>
2020-07-04 12:43:43 -0700

    proxy complete

--- server/index.js
+++ server/index.js
@@ -11,9 +11,9 @@ app.use(express.static(path.join(__dirname, '../client')));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.all('/calendar', createProxyMiddleware({ target: 'localhost:3001' }));
-app.all('/reviews', createProxyMiddleware({ target: 'localhost:3002' }));
-app.all('/carousel', createProxyMiddleware({ target: 'localhost:3003' }));
-app.all('/photogallery', createProxyMiddleware({ target: 'localhost:3004' }));
+app.use('/api', createProxyMiddleware({ target: 'http://52.14.226.255/', changeOrigin: true }));
+app.use('/reviews', createProxyMiddleware({ target: 'http://18.224.95.187/', changeOrigin: true }));
+app.use('/carousel', createProxyMiddleware({ target: 'http://54.215.84.53/', changeOrigin: true }));
+app.use('/photogallery', createProxyMiddleware({ target: 'http://ec2-18-221-5-8.us-east-2.compute.amazonaws.com:3004/', changeOrigin: true }));
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}/`));
